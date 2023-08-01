b7c724ca3ae61f6b120802b5eccd4d7e3db66462
Ruderson Florentino <rudersonvf@gmail.com>
2023-08-01 00:43:39 -0300

    update: Component button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import "./styles/app.scss";
+import "./styles/App.scss";
 import AppRoutes from "./routes";
 
 function App() {
--- src/components/CardFilter/index.tsx
+++ src/components/CardFilter/index.tsx
@@ -1,18 +1,39 @@
 import { Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import styles from "./styles.module.scss";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import SortByAlphaIcon from "@mui/icons-material/SortByAlpha";
 
 export default function CardFilter() {
+  const options = [
+    { label: "Ação", id: 1 },
+    { label: "Aventura", id: 2 },
+    { label: "Fantasia", id: 3 },
+    { label: "Sci-Fi", id: 4 },
+  ];
+
   return (
     <div className={styles["filter-card-container"]}>
-      <input
-        className={styles["input-filter"]}
-        type="text"
-        placeholder="Digite o nome do filme"
-      />
-      <Button className={styles["btn-search"]} variant="contained">
-        <SearchIcon className={styles["btn-icon"]} />
-      </Button>
+      <div className={styles["filter-search-container"]}>
+        <input
+          className={styles["input-filter"]}
+          type="text"
+          placeholder="Digite o nome do filme"
+        />
+        <Button className={styles["btn-search"]} variant="contained">
+          <SearchIcon className={styles["btn-icon"]} />
+        </Button>
+      </div>
+      <div className={styles["filter-btn-container"]}>
+        <Button className={styles["filter-btn"]}>
+          <FilterAltIcon />
+          <span>Filtro</span>
+        </Button>
+        <Button className={styles["filter-btn"]}>
+          <SortByAlphaIcon />
+          <span>Ordenar</span>
+        </Button>
+      </div>
     </div>
   );
 }
