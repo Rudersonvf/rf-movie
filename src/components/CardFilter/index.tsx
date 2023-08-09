99a91f010e5b9d94a24733af844a6366ff3b8369
Ruderson Rolemar <suporte2@rolemar.com>
2023-08-09 17:56:20 -0300

    update

--- src/components/CardFilter/index.tsx
+++ src/components/CardFilter/index.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@mui/material";
+import { Button, InputLabel, MenuItem, Select } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import styles from "./styles.module.scss";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import SortByAlphaIcon from "@mui/icons-material/SortByAlpha";
 import { useState } from "react";
+import { CategoryDTO } from "../../models/category";
 
 type FormData = {
   inptName?: string;
@@ -11,9 +12,10 @@ type FormData = {
 
 type Props = {
   onFilter: Function;
+  categories: CategoryDTO[];
 };
 
-export default function CardFilter({ onFilter }: Props) {
+export default function CardFilter({ onFilter, categories }: Props) {
   const [formData, setFormData] = useState<FormData>({});
 
   function handleChange(event: any) {
@@ -52,12 +54,18 @@ export default function CardFilter({ onFilter }: Props) {
       <div className={styles["filter-btn-container"]}>
         <Button className={styles["filter-btn"]}>
           <FilterAltIcon />
-          <span>Filtro</span>
+          <span>Categorias</span>
         </Button>
-        <Button className={styles["filter-btn"]}>
-          <SortByAlphaIcon />
-          <span>Ordenar</span>
-        </Button>
+
+        <select required className={styles["filter-btn"]}>
+          <option value="" disabled selected hidden>
+            Ordernar
+          </option>
+          <option>
+            <FilterAltIcon /> Male
+          </option>
+          <option>Female</option>
+        </select>
       </div>
     </div>
   );
